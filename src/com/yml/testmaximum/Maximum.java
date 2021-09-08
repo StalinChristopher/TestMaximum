ec2676c69491e0b1bd7ec1971358428dade7498a
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-08 23:17:41 +0530

    Added logic to find maximum of 3 Integer numbers

--- src/com/yml/testmaximum/Maximum.java
+++ src/com/yml/testmaximum/Maximum.java
@@ -2,4 +2,19 @@ package com.yml.testmaximum;
 
 public class Maximum {
 	
+	public void maximum(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if(y.compareTo(max)>0) {
+			max = y;
+		}
+		if(z.compareTo(max)>0){
+			max = z;
+		}
+		
+		printIntegerMax(x,y,z,max);
+	}
+
+	private void printIntegerMax(Integer x, Integer y, Integer z, Integer max) {
+		System.out.println("Max of "+x+", "+y+", "+z+" is "+max);
+	}
 }
--- src/com/yml/testmaximum/MaximumMain.java
+++ src/com/yml/testmaximum/MaximumMain.java
@@ -3,7 +3,12 @@ package com.yml.testmaximum;
 public class MaximumMain {
 
 	public static void main(String[] args) {
-		
+		Integer x = 100, y = 50, z = 20;
+		Integer a = 40, b = 80, c = 50;
+		Integer i = 30, j = 50, k = 90;
+		new Maximum().maximum(x, y, z);
+		new Maximum().maximum(a, b, c);
+		new Maximum().maximum(i, j, k);
 	}
 
 }
