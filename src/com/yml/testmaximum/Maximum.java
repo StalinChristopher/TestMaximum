30a519baa3290418e990411f9d170a5858648302
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-08 22:59:44 +0530

    Initialized java project

---
+++ src/com/yml/testmaximum/Maximum.java
@@ -0,0 +1,5 @@
+package com.yml.testmaximum;
+
+public class Maximum {
+	
+}
---
+++ src/com/yml/testmaximum/MaximumMain.java
@@ -0,0 +1,9 @@
+package com.yml.testmaximum;
+
+public class MaximumMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
